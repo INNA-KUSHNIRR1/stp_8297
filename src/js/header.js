5f8768c2ae6b48c7391a8b57d7d69cc0ce4a5f8f
Inna Kushnir <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2025-03-16 15:37:46 +0100

    add menu scss

--- src/js/header.js
+++ src/js/header.js
@@ -3,21 +3,32 @@ document.addEventListener('DOMContentLoaded', function () {
   const menuBackdrop = document.querySelector('.burger-backdrop');
   const menu = document.querySelector('.burger-menu');
   const closeButton = document.querySelector('.menu-close-btn');
+  const items = document.querySelectorAll('.menu-item');
 
   // Open menu
   burgerBtn.addEventListener('click', function () {
-    menuBackdrop.classList.add('open');
+    menuBackdrop.classList.add('is-open');
+    menu.classList.add('is-open');
   });
 
   // Close menu
   closeButton.addEventListener('click', function () {
-    menuBackdrop.classList.remove('open');
+    menuBackdrop.classList.remove('is-open');
+    menu.classList.remove('is-open'); // Удаляем класс у меню
   });
 
+  items.forEach(item => {
+    item.addEventListener('click', closeMenu);
+  });
+
+  function closeMenu() {
+    menuBackdrop.classList.remove('is-open');
+    menu.classList.remove('is-open');
+  }
   // Close by clicking outside menu
   menuBackdrop.addEventListener('click', function (event) {
     if (!menu.contains(event.target) && !burgerBtn.contains(event.target)) {
-      menuBackdrop.classList.remove('open');
+      menuBackdrop.classList.remove('is-open');
     }
   });
 });
