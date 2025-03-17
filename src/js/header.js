2ac674758a58ca3b20ddb46673d0780c1a7385cc
Inna Kushnir <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2025-03-17 10:09:45 +0100

    comment backdrop

--- src/js/header.js
+++ src/js/header.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
   const burgerBtn = document.querySelector('.btn-burger');
-  const menuBackdrop = document.querySelector('.burger-backdrop');
+  // const menuBackdrop = document.querySelector('.burger-backdrop');
   const menu = document.querySelector('.burger-menu');
   const closeButton = document.querySelector('.menu-close-btn');
   const items = document.querySelectorAll('.menu-item');
 
   // Open menu
   burgerBtn.addEventListener('click', function () {
-    menuBackdrop.classList.add('is-open');
+    // menuBackdrop.classList.add('is-open');
     menu.classList.add('is-open');
   });
 
   // Close menu
   closeButton.addEventListener('click', function () {
-    menuBackdrop.classList.remove('is-open');
+    // menuBackdrop.classList.remove('is-open');
     menu.classList.remove('is-open'); //Removing the class from the menu
   });
 
@@ -22,13 +22,18 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   function closeMenu() {
-    menuBackdrop.classList.remove('is-open');
+    // menuBackdrop.classList.remove('is-open');
     menu.classList.remove('is-open');
   }
-  // Close by clicking outside menu
-  menuBackdrop.addEventListener('click', function (event) {
-    if (!menu.contains(event.target) && !burgerBtn.contains(event.target)) {
-      menuBackdrop.classList.remove('is-open');
-    }
+
+  menu.addEventListener('click', function () {
+    menu.classList.remove('is-open');
   });
+
+  // Close by clicking outside menu
+  // menuBackdrop.addEventListener('click', function (event) {
+  //   if (!menu.contains(event.target) && !burgerBtn.contains(event.target)) {
+  //     menuBackdrop.classList.remove('is-open');
+  //   }
+  // });
 });
