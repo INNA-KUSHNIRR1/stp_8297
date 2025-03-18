b5305e2f126f0131a5cb4f3a985ace426a11136f
Inna Kushnir <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2025-03-18 22:21:24 +0100

    update swiper in reviews

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -4,14 +4,54 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-if (window.innerWidth <= 767) {
-  var swiper = new Swiper('.mySwiperr', {
-    slidesPerView: 'auto',
-    spaceBetween: 12,
-    centeredSlides: true,
-    pagination: {
-      el: '.reviews-swiper-pagination',
-      clickable: true,
-    },
-  });
-}
+document.addEventListener('DOMContentLoaded', function () {
+  function initSwiper() {
+    return new Swiper('.reviews-swiper', {
+      loop: true,
+      slidesPerView: 'auto',
+      spaceBetween: 12,
+      centeredSlides: true,
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+      },
+    });
+  }
+
+  let swiperInstance; // Для хранения экземпляра Swiper
+
+  function checkScreenSize() {
+    const isMobile = window.innerWidth < 1199;
+    const swiperContainer = document.querySelector('.reviews-swiper');
+
+    if (isMobile) {
+      if (!swiperInstance) {
+        swiperContainer.classList.add('swiper');
+        document.querySelector('.reviews-list').classList.add('swiper-wrapper');
+        document.querySelectorAll('.reviews-item').forEach(slide => {
+          slide.classList.add('swiper-slide');
+        });
+
+        swiperInstance = initSwiper();
+      }
+    } else {
+      if (swiperInstance) {
+        swiperInstance.destroy(true, true);
+        swiperInstance = null;
+      }
+
+      swiperContainer?.classList.remove('swiper');
+      document
+        .querySelector('.reviews-list')
+        ?.classList.remove('swiper-wrapper');
+      document.querySelectorAll('.reviews-item').forEach(slide => {
+        slide.classList.remove('swiper-slide');
+      });
+    }
+  }
+
+  checkScreenSize();
+  window.addEventListener('resize', checkScreenSize);
+});
