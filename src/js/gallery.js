9e04b005aa12afe13735df2ccc360b2cdcb82481
Inna Kushnir <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2025-03-19 10:07:59 +0100

    Update gallery.js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -4,16 +4,20 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-const swiper = new Swiper('.gallery-swiper', {
-  modules: [Pagination, Navigation],
-  direction: 'horizontal',
-  // loop: true,
-  slidesPerView: 2,
-  spaceBetween: 6,
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.gallery-swiper', {
+    modules: [Pagination, Navigation],
+    direction: 'horizontal',
+    // loop: true,
+    slidesPerView: 2,
+    spaceBetween: 6,
 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true,
-  },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    observer: true,
+    observeParents: true,
+  });
 });
