0d7b1278c3279beb6f284f5450905d90c48c66d3
Inna Kushnir <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2025-03-20 20:41:07 +0100

    update modal cookies

--- src/js/modal-cookies.js
+++ src/js/modal-cookies.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   const modalCookies = document.querySelector('.modal-cookies');
-  const cookieBox = document.querySelector('.modal-cookies-wrapper');
   const acceptBtn = document.querySelector('#acceptBtn');
   const declineBtn = document.querySelector('.button.decline');
 
@@ -11,17 +10,19 @@ document.addEventListener('DOMContentLoaded', () => {
     modalCookies.style.display = 'none';
   } else {
     modalCookies.style.display = 'flex';
-    cookieBox.classList.add('show'); // Adding a class to show the modal
+    setTimeout(() => {
+      modalCookies.classList.add('show');
+    }, 1000); // Adding a class to show the modal
     document.body.style.overflow = 'hidden'; // Blocking scrolling
   }
 
   // Function to close the modal window
   const closeModal = () => {
-    cookieBox.classList.remove('show');
+    modalCookies.classList.remove('show');
     document.body.style.overflow = ''; // Unblocking scrolling
     setTimeout(() => {
       modalCookies.style.display = 'none';
-    }, 300);
+    }, 500);
   };
 
   // Handler for the "Accept Cookies" button click
